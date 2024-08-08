a5a8fbe4c76f6eb8a3632da41bb201463b12573e
akirarika <33272184+akirarika@users.noreply.github.com>
2024-08-08 10:44:18 +0800

    🎈 publish: v2.0.0-alpha.1

--- src/app.ts
+++ src/app.ts
@@ -50,50 +50,68 @@ export const initSharedRef = (options: SharedRefOptions) => {
 };
 
 export const sharedRef = async <T>(options: { key: string; value: T; meta?: Record<string, any> }): Promise<Ref<T>> => {
-  if (!worker) throw new Error("[SharedRef] Shared worker not initialized, call initSharedRef(...) first.");
-  await workerReady.promise;
-  const resolvers = withResolvers<T>();
-  const id = `${options.key}_${nanoid()}`;
-  waitingGet.set(id, resolvers);
-  worker!.postMessage({
-    type: "GET",
-    key: options.key,
-    meta: options.meta ?? {},
-    id: id,
-  } satisfies SharedRefMessageGet);
-  let value: any = undefined;
-  const result = (await resolvers.promise) as unknown as SharedRefMessageResult;
-  if (result.empty === false) {
-    value = result.value;
+  if (typeof window !== "undefined" && typeof window?.document?.createElement !== "undefined") {
+    if (!worker) throw new Error("[SharedRef] Shared worker not initialized, call initSharedRef(...) first.");
+    await workerReady.promise;
+    const resolvers = withResolvers<T>();
+    const id = `${options.key}_${nanoid()}`;
+    waitingGet.set(id, resolvers);
+    worker!.postMessage({
+      type: "GET",
+      key: options.key,
+      meta: options.meta ?? {},
+      id: id,
+    } satisfies SharedRefMessageGet);
+    let value: any = undefined;
+    const result = (await resolvers.promise) as unknown as SharedRefMessageResult;
+    if (result.empty === false) {
+      value = result.value;
+    } else {
+      value = options.value;
+    }
+
+    const refController = {} as RefController<any>;
+    refController.ref = customRef((track, trigger) => {
+      refController.track = track;
+      refController.trigger = trigger;
+      refController.value = value;
+      return {
+        get() {
+          refController.track();
+          return refController.value;
+        },
+        set(newValue) {
+          refController.value = newValue;
+          refController.trigger();
+          worker!.postMessage({
+            type: "SET",
+            key: options.key,
+            meta: options.meta ?? {},
+            value: newValue,
+          } satisfies SharedRefMessageSet);
+        },
+      };
+    });
+    if (refs.has(options.key)) throw new Error(`[SharedRef] Multiple sharedRefs are using the same key: '${options.key}'.`);
+
+    refs.set(options.key, refController);
+
+    return refController.ref;
   } else {
-    value = options.value;
+    const cref = customRef((track, trigger) => {
+      let value = options.value;
+      return {
+        get() {
+          track();
+          return value;
+        },
+        set(newValue) {
+          value = newValue;
+          trigger();
+        },
+      };
+    });
+
+    return cref;
   }
-
-  const refController = {} as RefController<any>;
-  refController.ref = customRef((track, trigger) => {
-    refController.track = track;
-    refController.trigger = trigger;
-    refController.value = value;
-    return {
-      get() {
-        refController.track();
-        return refController.value;
-      },
-      set(newValue) {
-        refController.value = newValue;
-        refController.trigger();
-        worker!.postMessage({
-          type: "SET",
-          key: options.key,
-          meta: options.meta ?? {},
-          value: newValue,
-        } satisfies SharedRefMessageSet);
-      },
-    };
-  });
-  if (refs.has(options.key)) throw new Error(`[SharedRef] Multiple sharedRefs are using the same key: '${options.key}'.`);
-
-  refs.set(options.key, refController);
-
-  return refController.ref;
 };
